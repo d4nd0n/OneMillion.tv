0e8e807ac534e241e72f214643fa67d5eb4a2525
d4nd0n <napo9829@gmail.com>
2022-08-09 18:38:12 +0200

    4 files modified

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
-  styleUrls: ['./video.component.css']
+  styleUrls: ['./video.component.css'],
 })
 export class VideoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
+  name = 'Video events';
+  videoSource =
+    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4';
 
-  ngOnInit() {
+  @ViewChild('videoPlayer') videoplayer: any;
+  public startedPlay: boolean = false;
+  public show: boolean = false;
+  pauseVideo(videoplayer) {
+    videoplayer.nativeElement.play();
+    // this.startedPlay = true;
+    // if(this.startedPlay == true)
+    // {
+    setTimeout(() => {
+      videoplayer.nativeElement.pause();
+      if (videoplayer.nativeElement.paused) {
+        this.show = !this.show;
+      }
+    }, 5000);
+    // }
   }
 
+  closebutton(videoplayer) {
+    this.show = !this.show;
+    videoplayer.nativeElement.play();
+  }
 }
