48c33dce89d44185535e3f2162f269c0e6b6b37a
d4nd0n <napo9829@gmail.com>
2022-08-25 12:30:39 +0200

    5 files modified

--- src/app/components/clip/clip.component.ts
+++ src/app/components/clip/clip.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+  styleUrls: ['./clip.component.css'],
 })
 export class ClipComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.clipLoad();
   }
 
+  imagePath;
+
+  clipLoad = (): void => {
+    let loadtv = 'https://i.postimg.cc/mk3Ffj3V/tvstatic.gif';
+    let orange = 'https://i.postimg.cc/c4JPKsTm/tv.png';
+    this.imagePath = loadtv;
+
+    setTimeout(() => {
+      console.log('CIAO');
+      this.imagePath = orange;
+    }, 5000);
+  };
 }
+
+
--- src/app/components/clipcontroller/clipcontroller.component.ts
+++ src/app/components/clipcontroller/clipcontroller.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-clipcontroller',
   templateUrl: './clipcontroller.component.html',
-  styleUrls: ['./clipcontroller.component.css']
+  styleUrls: ['./clipcontroller.component.css'],
 })
 export class ClipcontrollerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  clipsLoad = (): void => {};
+}
+let loadtv = 'https://i.postimg.cc/mk3Ffj3V/tvstatic.gif';
+let orange = 'https://i.postimg.cc/c4JPKsTm/tv.png';
+
+const CLIPS = [
+  { name: 'First', views: 100000, likes: 14000, imagePath: orange },
+  { name: 'Second', views: 100000, likes: 14000, imagePath: orange },
+  { name: 'Third', views: 100000, likes: 14000, imagePath: orange },
+  { name: 'Fourth', views: 100000, likes: 14000, imagePath: orange },
+  { name: 'Fifth', views: 100000, likes: 14000, imagePath: orange },
+];
+
+export class Clips {
+  clips = CLIPS;
+
+  trackHero(index, clip) {
+    console.log(clip);
+    return clip ? clip.id : undefined;
   }
-
 }
