3a9303c4ecb5a6a3acde202c345d6344f325462c
d4nd0n <napo9829@gmail.com>
2022-08-26 18:29:59 +0200

    3 files modified

--- src/app/components/user/user-top-clip/user-top-clip.component.ts
+++ src/app/components/user/user-top-clip/user-top-clip.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {
   animate,
+  query,
   stagger,
   state,
   style,
@@ -15,8 +16,23 @@ import {
   styleUrls: ['./user-top-clip.component.css'],
   animations: [
     trigger('showState', [
-      transition('hide => show', []),
-      transition('show => hide', []),
+      state('show', style({ opacity: 1, height: '*' })),
+      state('hide', style({ opacity: 1, height: '*' })),
+      transition('hide => show', [
+        query('.clipRow', [
+          style({ opacity: 1, transform: 'translateY(-100px)' }),
+          stagger(30, [
+            animate(
+              '500ms cubic-bezier(0.35, 0, 0.25, 1)',
+              style({ opacity: 1, transform: 'none' })
+            ),
+          ]),
+        ]),
+      ]),
+      transition('show => hide', [
+        style({ opacity: 0, height: '*' }),
+        animate('300ms ease-in', style({ opacity: 1, height: '*' })),
+      ]),
     ]),
   ],
 })
